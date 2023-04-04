cd3ab8c63a12e13d0e820e2d38d8556820904215
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-04-04 01:57:40 -0700

    4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 $('.popular_slider').slick({
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 2000,
     arrows: false,
+    centerMode: true,
+    centerPadding: 0,
   });
+
+  $('.count').counterUp({
+    delay: 10,
+    time: 1000
+});
+// Testimonial slider start 
+$('.testi_slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  dots: true,
+});
+// Testimonial slider end
--- script.js
+++
@@ -1,9 +0,0 @@
-$('.popular_slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: false,
-    centerMode: true,
-    centerPadding: 0,
-  });
