ac3a1d94d1221e5a0720d2d0576ec869016e8fde
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-04-09 23:24:07 -0700

    accordion

--- js/script.js
+++ js/script.js
@@ -7,17 +7,19 @@ $('.popular_slider').slick({
     centerMode: true,
     centerPadding: 0,
   });
-
-  $('.count').counterUp({
-    delay: 10,
-    time: 1000
+// Counter part start
+$('.count').counterUp({
+  delay: 10,
+  time: 1000
 });
+// Counter part end
 // Testimonial slider start 
-$('.testi_slider').slick({
+$('.testi_slide').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: false,
   autoplaySpeed: 2000,
   dots: true,
+  arrows: false,
 });
 // Testimonial slider end
